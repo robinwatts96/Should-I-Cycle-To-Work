c43aedd37e155a790c79139ed7bf13a145c96392
robinwatts96 <robinwatts96@gmail.com>
2022-01-19 07:15:23 +0000

    About to restructure to add postcode

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-document.getElementById('answer').innerHTML = "howdy";
-alert('hey');
-console.log('holla');
+api.openweathermap.org/data/2.5/weather?zip=bn2,gb&appid=36b033476dd5cea0c38cb227411b005f
