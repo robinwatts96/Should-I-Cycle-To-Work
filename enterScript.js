f2d09dee615eecb87681c3285aebab9ff6b844b0
robinwatts96 <robinwatts96@gmail.com>
2022-01-20 07:07:39 +0000

    enter page added

---
+++ enterScript.js
@@ -0,0 +1,8 @@
+const postCode = document.getElementById('post-code');
+const submitPostCode = document.getElementById('submit-post-code');
+let postCodeValue;
+
+submitPostCode.addEventListener('click', function savePostCode () {
+    postCodeValue = postCode.value;
+    console.log(postCodeValue);
+})
--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-api.openweathermap.org/data/2.5/weather?zip=bn2,gb&appid=36b033476dd5cea0c38cb227411b005f
+
